640bbc7b6477c1aa7721cb1a5049f05a4f1625fa
Mainthan Suriyakumaran <mainthan2@gmail.com>
2023-04-02 21:28:55 +0100

    auth-service: add tests

---
+++ auth-service/tests/authenticationRoutes.test.js
@@ -0,0 +1,69 @@
+const supertest = require('supertest')
+const sinon = require('sinon');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const axios = require('axios')
+const app = require('../app')
+const authController = require('../controllers/authenticationController')
+
+const api = supertest(app)
+
+describe('authentication routes', () => {
+  let fakeUser
+  let getStub
+
+  beforeAll(() => {
+    fakeUser = {
+      _id: '1234',
+      username: 'testuser',
+      password: 'testpassword',
+    }
+  })
+
+  beforeEach(() => {
+    getStub = sinon.stub(axios, 'get')
+  })
+
+  afterEach(() => {
+    getStub.restore()
+  })
+
+  describe('POST /login', () => {
+    it('should return a 200 with a jwt token after a successful login', async () => {
+      const hashedPassword = await bcrypt.hash(fakeUser.password, 10)
+      const getData = { ...fakeUser, password: hashedPassword }
+      getStub.resolves({ data: getData })
+
+      const res = await api
+            .post('/login')
+            .send(fakeUser)
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body.token).toBeDefined()
+    })
+
+    it('should return a 401 when a user is not found in the database', async () => {
+      getStub.resolves({ data: null })
+      const res = await api
+            .post('/login')
+            .send(fakeUser)
+
+      expect(res.statusCode).toEqual(401)
+    })
+
+    it('should return a 401 when the user exists but the password is wrong', async () => {
+      const hashedPassword = await bcrypt.hash('differentpassword', 10)
+      const getData = { ...fakeUser, password: hashedPassword }
+      getStub.resolves({ data: getData })
+
+      const res = await api
+            .post('/login')
+            .send(fakeUser)
+
+      expect(res.statusCode).toEqual(401)
+    })
+
+
+  })
+
+})
