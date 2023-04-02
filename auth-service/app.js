4987b75e123d656e1c7564c16eadf7d40395f13e
Mainthan Suriyakumaran <mainthan2@gmail.com>
2023-04-02 22:15:58 +0100

    auth-service: use default port

--- auth-service/app.js
+++ auth-service/app.js
@@ -9,8 +9,8 @@ app.use(express.json())
 app.use('/', authenticationRoutes)
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`)
 })
 
 module.exports = app
