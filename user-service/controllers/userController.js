6ca94bb48362af0beae5e29abb4872efb41fe060
Mainthan Suriyakumaran <mainthan2@gmail.com>
2023-04-03 20:37:46 +0100

    user-service: add controller functions getAllUsers and createUser

--- user-service/controllers/userController.js
+++ user-service/controllers/userController.js
@@ -18,5 +18,46 @@ const getUser = async (req, res) => {
     }
 
     return res.status(201).json({ userId: user._id, username: user.username })
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ message: 'Internal server error' })
   }
+}
+
+const getAllUsers = async (req, res) => {
+  try {
+    const response = await axios.get(`${databaseUrl}/users/`)
+    const users = response.data.map(user => { userId: user._id, username: user.username })
+
+    return res.status(200).json(users)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
+
+const createUser = async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    if (!username || !password || username.length < 3 || password.length < 3) {
+      return res.status(400).json({ message: 'Invalid username or password' })
+    }
+
+    const response = await axios.post(`${databaseUrl}/users?username=${username}&password=${password}`)
+    const user = response.data
+  } catch(err) {
+    if (err.response && err.response.status === 409) {
+      res.status(409).json({ message: 'Username is taken' })
+    } else {
+      console.error(err)
+      res.status(500).json({ message: 'Internal server error' })
+    }
+  }
+}
+
+module.exports = {
+  getUser,
+  getAllUsers,
+  createUser,
 }
